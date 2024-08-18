ca33affe83f23e49922ef99d82d30b28708c9ab8
cpumaxx <163466046+cpumaxx@users.noreply.github.com>
2024-08-17 21:38:24 -0700

    Update run_recap_bot.sh
    overdue update. change main to llama-cli and other logging bits

--- run_recap_bot.sh
+++ run_recap_bot.sh
@@ -9,9 +9,9 @@ MODEL=/opt/text-generation-webui/models/miqu-70b-q5/miqu-1-70b.q5_K_M.gguf
 
 cp ./thread_summary.prompt /tmp/thread.json
 
-curl -s https://a.4cdn.org/g/thread/$THREAD.json | jq '[.posts[] | { "no" : .no, "com" : .com }][2:999]' | sed 's/".*ecap.*"/"com": ""/g' | sed 's/".*Recent Highlights.*"/"com": ""/g' | jq . -cr | sed "s/<br>/ /g" | perl -MHTML::Entities -pe 'decode_entities($_);' | perl -pe 's{\n}{ }g' | perl -pe 's{>}{>\n}g' | perl -pe 's{<}{\n<}g' | grep -v '<' | grep -v '^\s*$' |  tr -d "\n" >> /tmp/thread.json
+curl -s https://a.4cdn.org/g/thread/$THREAD.json | jq '[.posts[] | { "comment" : .com, "replynum" : .no }][2:999]' | sed 's/".*ecap.*"/"com": ""/g' | sed 's/".*Recent Highlights.*"/"com": ""/g' | jq . -cr | sed "s/<br>/ /g" | perl -MHTML::Entities -pe 'decode_entities($_);' | perl -pe 's{\n}{ }g' | perl -pe 's{>}{>\n}g' | perl -pe 's{<}{\n<}g' | grep -v '<' | grep -v '^\s*$' |  tr -d "\n" >> /tmp/thread.json
 
-/usr/src/llama.cpp.master_clean/main --log-disable --log-file /tmp/recapbot.log -m $MODEL --no-display-prompt -c 32768 -t 55 -f /tmp/thread.json 2>/dev/null | tee -a ~/lmg_recap.log
+time /usr/src/llama.cpp.master.clean/llama-cli --log-disable --log-file /tmp/recapbot.log -s 39555 -m $MODEL --no-display-prompt  -t 55 -c 32768 --numa distribute -f /tmp/thread.json 2> /tmp/recap_stats.log | tee -a ~/lmg_recap.log
 
 rm /tmp/thread.json
 
